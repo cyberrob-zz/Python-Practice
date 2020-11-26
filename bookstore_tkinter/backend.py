50c7dce00627c65353dbb75bb71d43fdcd3bc519
Robert Wang <robertwang.tw@gmail.com>
2020-11-26 14:22:59 +0000

    Let backend.py as class imported into main.py to work as class

--- bookstore_tkinter/backend.py
+++ bookstore_tkinter/backend.py
@@ -1,57 +1,35 @@
 import sqlite3
 
-def connect():
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn text)')
-    conn.commit()
-    conn.close()
+class Database:
 
-def insert(title, author, year, isbn):
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO book VALUES (NULL, ?, ?, ?, ?)', (title, author, year, isbn))
-    conn.commit()
-    conn.close()
+  def __init__(self, db):
+    self.conn = sqlite3.connect(db)
+    self.cursor = self.conn.cursor()
+    self.cursor.execute('CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn text)')
+    self.conn.commit()
 
-def view_all():
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM book')
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
+  def insert(self, title, author, year, isbn):
+      self.cursor.execute('INSERT INTO book VALUES (NULL, ?, ?, ?, ?)', (title, author, year, isbn))
+      self.conn.commit()
 
-def search(title="", author="", year="", isbn=""):
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
+  def view_all(self):
+      self.cursor.execute('SELECT * FROM book')
+      rows = self.cursor.fetchall()
+      return rows
 
-def delete(id):
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM book WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
+  def search(self, title="", author="", year="", isbn=""):
+      self.cursor.execute("SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
+      rows = self.cursor.fetchall()
+      return rows
 
-def update(id, title="", author="", year="", isbn=""):
-    conn = sqlite3.connect('books.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+  def delete(self, id):
+      self.cursor.execute("DELETE FROM book WHERE id = ?", (id,))
+      self.conn.commit()
 
-# def clear():
+  def update(self, id, title="", author="", year="", isbn=""):
+      self.cursor.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
+      self.conn.commit()
 
+  def __del__(self):
+    self.conn.close()
 
-connect()
-# insert(title='Old man and the sea', author='Ernest Hemingway', year=1954, isbn='1781396809')
-# print(search(author='Ernest Hemingway'))
-# delete(4)
-# print(view_all())
-
-# update(id=1, title="Young man and sear", author="Matt Damon", year=1999, isbn="9183492837134")
-# print(view_all())
--- bookstore_tkinter/main.py
+++ bookstore_tkinter/main.py
@@ -1,138 +1,144 @@
 from tkinter import *
-import backend
+from backend import Database
+
+database = Database("bookstore.db")
+
+class BookStoreMgrWindow(object):
+
+  def __init__(self, window):
+    self.window = window
+
+    self.window.wm_title('Book Store')
+
+    title_label = Label(master=window, text="Title")
+    title_label.grid(row=0, column=0)
+
+    author_label = Label(master=window, text="Author")
+    author_label.grid(row=0, column=2)
+
+    year_label = Label(master=window, text="year")
+    year_label.grid(row=1, column=0)
+
+    isbn_label = Label(master=window, text="ISBN")
+    isbn_label.grid(row=1, column=2)
+
+    self.book_count_text = StringVar()
+    self.book_count_text.set("0 books in stock")
+    self.book_count_label = Label(master=window, textvariable=self.book_count_text)
+    self.book_count_label.grid(row=8, column=0, columnspan=4)
 
 
-def get_selected_row(event):
-  # mark selected_row as global variable so that other function can access it.
-  global selected_row
-  index = -1
-  if len(book_list.curselection()) > 0:
-    index = book_list.curselection()[0]
-    selected_row = book_list.get(index)
+    # Entries
+    self.title_text = StringVar()
+    self.title_input = Entry(master=window, textvariable=self.title_text)
+    self.title_input.grid(row=0, column=1)
+
+    self.author_text = StringVar()
+    self.author_input = Entry(master=window, textvariable=self.author_text)
+    self.author_input.grid(row=0, column=3)
+
+    self.year_text = StringVar()
+    self.year_input = Entry(master=window, textvariable=self.year_text)
+    self.year_input.grid(row=1, column=1)
+
+    self.isbn_text = StringVar()
+    self.isbn_input = Entry(master=window, textvariable=self.isbn_text)
+    self.isbn_input.grid(row=1, column=3)
+
+    # list
+    self.book_list = Listbox(master=window, height=6, width=35)
+    self.book_list.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+    self.book_list.bind('<<ListboxSelect>>', self.get_selected_row)
+
+    # scrollbar
+    self.scrollbar = Scrollbar(master=window)
+    self.scrollbar.grid(row=2,column=2, rowspan=6)
+
+    self.book_list.configure(yscrollcommand=self.scrollbar.set)
+    self.scrollbar.configure(command=self.book_list.yview)
+
+    # buttons
+    self.view_all = Button(master=window, text='View all', width=12, command=self.view_all_command)
+
+    self.view_all.grid(row=2, column=3)
+
+    self.search_entry = Button(master=window, text='Search entry', width=12, command=self.search_command)
+    self.search_entry.grid(row=3, column=3)
+
+    self.add_entry = Button(master=window, text='Add entry', width=12, command=self.insert_command)
+    self.add_entry.grid(row=4, column=3)
+
+    self.update_entry = Button(master=window, text='Update entry', width=12, command=self.update_command)
+    self.update_entry.grid(row=5, column=3)
+
+    self.delete_entry = Button(master=window, text='Delete entry', width=12, command=self.delete_command)
+    self.delete_entry.grid(row=6, column=3)
+
+    self.close = Button(master=window, text='Close', width=12, command=window.destroy)
+    self.close.grid(row=7, column=3)
+
+    # Call view_all_command to load all books at start
+    self.view_all_command()
+
+  def get_selected_row(self, event):
+    # mark selected_row as global variable so that other function can access it.
+    # global selected_row
+    index = -1
+    if len(self.book_list.curselection()) > 0:
+      index = self.book_list.curselection()[0]
+      self.selected_row = self.book_list.get(index)
+    
+    # show selected_row onto input fields
+    self.title_input.delete(0, END)
+    self.title_input.insert(END, self.selected_row[1])
+    self.author_input.delete(0, END)
+    self.author_input.insert(END, self.selected_row[2])
+    self.year_input.delete(0, END)
+    self.year_input.insert(END, self.selected_row[3])
+    self.isbn_input.delete(0, END)
+    self.isbn_input.insert(END, self.selected_row[4])
+    
+    return self.selected_row
+
+  def view_all_command(self):
+    self.book_list.delete(0, END)
+    rows = database.view_all()
+    self.book_count_text.set(f"{len(rows)} books in stock")
+    for row in rows:
+      self.book_list.insert(END, row)
+
+  def insert_command(self):
+    database.insert(self.title_text.get(), self.author_text.get(), self.year_text.get(), self.isbn_text.get())
+    self.view_all_command()
+      # Since select_row inserted, input fields should be cleared.
+    self.title_input.delete(0, END)
+    self.author_input.delete(0, END)
+    self.year_input.delete(0, END)
+    self.isbn_input.delete(0, END)
+
+  def search_command(self):
+    rows = database.search(self.title_text.get(), self.author_text.get(), self.year_text.get(), self.isbn_text.get())
+    self.book_list.delete(0, END)
+    for row in rows:
+      self.book_list.insert(END, row)
+
+  def delete_command(self):
+    database.delete(self.selected_row[0])
+    # Since select_row deleted, input fields should be cleared.
+    self.title_input.delete(0, END)
+    self.author_input.delete(0, END)
+    self.year_input.delete(0, END)
+    self.isbn_input.delete(0, END)
+    self.view_all_command()
+
+
+  def update_command(self):
+    database.update(self.selected_row[0], self.title_text.get(), self.author_text.get(), self.year_text.get(), self.isbn_text.get())
+    self.view_all_command()
+
   
-  # show selected_row onto input fields
-  title_input.delete(0, END)
-  title_input.insert(END, selected_row[1])
-  author_input.delete(0, END)
-  author_input.insert(END, selected_row[2])
-  year_input.delete(0, END)
-  year_input.insert(END, selected_row[3])
-  isbn_input.delete(0, END)
-  isbn_input.insert(END, selected_row[4])
-  
-  return selected_row
-  
-
-def view_all_command():
-  book_list.delete(0, END)
-  rows = backend.view_all()
-  book_count_text.set(f"{len(rows)} books in stock")
-  for row in rows:
-    book_list.insert(END, row)
-
-def insert_command():
-  backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
-  view_all_command()
-    # Since select_row inserted, input fields should be cleared.
-  title_input.delete(0, END)
-  author_input.delete(0, END)
-  year_input.delete(0, END)
-  isbn_input.delete(0, END)
-
-def search_command():
-  rows = backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
-  book_list.delete(0, END)
-  for row in rows:
-    book_list.insert(END, row)
-
-def delete_command():
-  backend.delete(selected_row[0])
-  # Since select_row deleted, input fields should be cleared.
-  title_input.delete(0, END)
-  author_input.delete(0, END)
-  year_input.delete(0, END)
-  isbn_input.delete(0, END)
-  view_all_command()
-
-
-def update_command():
-  backend.update(selected_row[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
-  view_all_command()
-
-  
-
 window = Tk()
-window.wm_title('Book Store')
-
-title_label = Label(master=window, text="Title")
-title_label.grid(row=0, column=0)
-
-author_label = Label(master=window, text="Author")
-author_label.grid(row=0, column=2)
-
-year_label = Label(master=window, text="year")
-year_label.grid(row=1, column=0)
-
-isbn_label = Label(master=window, text="ISBN")
-isbn_label.grid(row=1, column=2)
-
-book_count_text = StringVar()
-book_count_text.set("0 books in stock")
-book_count_label = Label(master=window, textvariable=book_count_text)
-book_count_label.grid(row=8, column=0, columnspan=4)
-
-
-# Entries
-title_text = StringVar()
-title_input = Entry(master=window, textvariable=title_text)
-title_input.grid(row=0, column=1)
-
-author_text = StringVar()
-author_input = Entry(master=window, textvariable=author_text)
-author_input.grid(row=0, column=3)
-
-year_text = StringVar()
-year_input = Entry(master=window, textvariable=year_text)
-year_input.grid(row=1, column=1)
-
-isbn_text = StringVar()
-isbn_input = Entry(master=window, textvariable=isbn_text)
-isbn_input.grid(row=1, column=3)
-
-# list
-book_list = Listbox(master=window, height=6, width=35)
-book_list.grid(row=2, column=0, rowspan=6, columnspan=2)
-
-book_list.bind('<<ListboxSelect>>', get_selected_row)
-
-# scrollbar
-scrollbar = Scrollbar(master=window)
-scrollbar.grid(row=2,column=2, rowspan=6)
-
-book_list.configure(yscrollcommand=scrollbar.set)
-scrollbar.configure(command=book_list.yview)
-
-# buttons
-view_all = Button(master=window, text='View all', width=12, command=view_all_command)
-
-view_all.grid(row=2, column=3)
-
-search_entry = Button(master=window, text='Search entry', width=12, command=search_command)
-search_entry.grid(row=3, column=3)
-
-add_entry = Button(master=window, text='Add entry', width=12, command=insert_command)
-add_entry.grid(row=4, column=3)
-
-update_entry = Button(master=window, text='Update entry', width=12, command=update_command)
-update_entry.grid(row=5, column=3)
-
-delete_entry = Button(master=window, text='Delete entry', width=12, command=delete_command)
-delete_entry.grid(row=6, column=3)
-
-close = Button(master=window, text='Close', width=12, command=window.destroy)
-close.grid(row=7, column=3)
-
-# Call view_all_command to load all books at start
-view_all_command()
-
+BookStoreMgrWindow(window)
 window.mainloop()
+
